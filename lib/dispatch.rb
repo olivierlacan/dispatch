15c0519738486cdba3f4581980609581b5ac7979
Olivier Lacan <hi@olivierlacan.com>
2018-10-24 13:50:02 -0400

    Add basic untested logic for config whitelist
    The idea here is to retrieve as many configuration options as possible
    without ever retrieving anything that could be including some sensitive
    information with PII or application secrets.
    
    You could say that technically Rails config should be secret-free since
    secrets.yml was introduced but that may not be the case.
    
    In the long-run it might make sense to offer a way to scan the app for
    high entropy strings and potentially private stuff like URIs.

--- lib/dispatch.rb
+++ lib/dispatch.rb
@@ -2,4 +2,28 @@ require "dispatch/version"
 require "dispatch/railtie"
 
 module Dispatch
+  WHITELIST = {
+    rails: {
+      configuration: [
+        :api_only,
+        :cache_store,
+        :console,
+        :log_level,
+        :force_ssl,
+        :file_watcher,
+        generators: [:options],
+        :public_file_server,
+        :time_zone,
+        :session_store,
+        session_options: [:cookie_only]
+      ]
+    }
+  }
+
+  def self.retrieve_basic_configuration(configuration)
+    configuration.each_with_object({}) do |config, object|
+      next unless WHITELIST.include?(config)
+      binding.pry
+    end
+  end
 end
--- lib/dispatch/railtie.rb
+++ lib/dispatch/railtie.rb
@@ -4,6 +4,7 @@ module Dispatch
   class Railtie < Rails::Railtie
     initializer "dispatch.send_anonymized_rails_metrics" do
       Rails.logger.info "===== DISPATCH ====="
+      Dispatch.retrieve_basic_configuration(Rails.configuration)
     end
   end
 end
